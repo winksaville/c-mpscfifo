fc5eee333c27d8a9699001709b2633a8e8af6095
Wink Saville <wink@saville.com>
2015-05-21 09:52:39 -0700

    Reformat hello.c using the clang-format.
    I added following line to .vimrc so its code can be reformatted in vim:
    
      $ map <C-I> :pyf ~/scripts/clang-format.py
    
    This is used by selecting some lines then pressing ctrl-I which places
    ":pyf ~/scripts/clang-format.py" in the vim command prompt. Pressing
    return then reformats the selected lines.

--- src/hello.c
+++ src/hello.c
@@ -2,8 +2,9 @@
 #include "msg.h"
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World! arc=%d\n", argc);
-	for (int i = 0; i < argc; i++) {
-	    printf(" argv[%d]=%s\n", i, argv[i]);
-	}
+  printf("Hello, World! arc=%d\n", argc);
+  for (int i = 0; i < argc; i++) {
+    printf(" argv[%d]=%s\n", i, argv[i]);
+  }
+  return 0;
 }
